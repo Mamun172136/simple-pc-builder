27f3dbc95ff8036bf11693c8ab4bd2fcd10cd2d5
Mamun172136 <ma1536204@gmail.com>
2023-10-03 12:24:23 +0600

    add  ram

--- src/pages/ram/ram.js
+++ src/pages/ram/ram.js
@@ -1,31 +1,31 @@
-// import Card from "@/components/shared/Card";
-// import React from "react";
+import Card from "@/components/shared/Card";
+import React from "react";
 
-// const ram = ({ motherboards }) => {
-//   return (
-//     <div>
-//       <h1 className="text-3xl p-4">Ram Product</h1>
-//       <div className=" p-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-//         {motherboards?.data.map((service) => (
-//           <Card key={service.id} service={service}></Card>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
+const ram = ({ motherboards }) => {
+  return (
+    <div>
+      <h1 className="text-3xl p-4">Ram Product</h1>
+      <div className=" p-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {motherboards?.data.map((service) => (
+          <Card key={service.id} service={service}></Card>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-// export const getStaticProps = async () => {
-//   // const res = await fetch("http://localhost:3000/api/news"); // internal API connected with mongoDB
-//   const res = await fetch("http://localhost:5000/rams"); // --> json server
-//   const data = await res.json();
-//   // console.log(data);
-//   return {
-//     props: {
-//       motherboards: data,
-//       // allNews: data.data, // when using internal API connected with mongoDB
-//     },
-//     revalidate: 10,
-//   };
-// };
+export const getStaticProps = async () => {
+  // const res = await fetch("http://localhost:3000/api/news"); // internal API connected with mongoDB
+  const res = await fetch("http://localhost:5000/rams"); // --> json server
+  const data = await res.json();
+  // console.log(data);
+  return {
+    props: {
+      motherboards: data,
+      // allNews: data.data, // when using internal API connected with mongoDB
+    },
+    revalidate: 10,
+  };
+};
 
-// export default ram;
+export default ram;

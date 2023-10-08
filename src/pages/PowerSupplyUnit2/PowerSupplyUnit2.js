5fc9888143ae2c51ad2a9a9184fb20c758fbdef3
Mamun172136 <ma1536204@gmail.com>
2023-10-09 02:49:26 +0600

    show powersuppllyunit  product using ssr

--- src/pages/PowerSupplyUnit2/PowerSupplyUnit2.js
+++ src/pages/PowerSupplyUnit2/PowerSupplyUnit2.js
@@ -1,36 +1,36 @@
-// import Card2 from "@/components/shared/Card2";
-// import React from "react";
-// import RootLayout from "../../../components/Layouts/RootLayout";
+import Card2 from "@/components/shared/Card2";
+import React from "react";
+import RootLayout from "../../../components/Layouts/RootLayout";
 
-// const PowerSupplyUnit2 = ({ motherboards }) => {
-//   return (
-//     <div>
-//       <h1 className="text-3xl p-4">Power Supply Unit Product</h1>
-//       <div className=" p-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-//         {motherboards?.data.map((service) => (
-//           <Card2 key={service.id} service={service}></Card2>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
+const PowerSupplyUnit2 = ({ motherboards }) => {
+  return (
+    <div>
+      <h1 className="text-3xl p-4">Power Supply Unit Product</h1>
+      <div className=" p-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {motherboards?.data.map((service) => (
+          <Card2 key={service.id} service={service}></Card2>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-// export const getServerSideProps = async () => {
-//   // const res = await fetch("http://localhost:3000/api/news"); // internal API connected with mongoDB
-//   const res = await fetch("http://localhost:5000/powerSupplyUnits"); // --> json server
-//   const data = await res.json();
-//   // console.log(data);
-//   return {
-//     props: {
-//       motherboards: data,
-//       // allNews: data.data, // when using internal API connected with mongoDB
-//     },
-//     // revalidate: 10,
-//   };
-// };
+export const getServerSideProps = async () => {
+  // const res = await fetch("http://localhost:3000/api/news"); // internal API connected with mongoDB
+  const res = await fetch("http://localhost:5000/powerSupplyUnits"); // --> json server
+  const data = await res.json();
+  // console.log(data);
+  return {
+    props: {
+      motherboards: data,
+      // allNews: data.data, // when using internal API connected with mongoDB
+    },
+    // revalidate: 10,
+  };
+};
 
-// PowerSupplyUnit2.getLayout = function getLayout(page) {
-//   return <RootLayout> {page} </RootLayout>;
-// };
+PowerSupplyUnit2.getLayout = function getLayout(page) {
+  return <RootLayout> {page} </RootLayout>;
+};
 
-// export default PowerSupplyUnit2;
+export default PowerSupplyUnit2;

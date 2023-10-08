acb61e0b770797b966197a85f81d7ee13d64af35
Mamun172136 <ma1536204@gmail.com>
2023-10-09 02:50:24 +0600

    show motherboard  product using ssr

--- src/pages/Motherboard2/Motherboard2.js
+++ src/pages/Motherboard2/Motherboard2.js
@@ -1,36 +1,36 @@
-// import Card from "@/components/shared/Card";
-// import Card2 from "@/components/shared/Card2";
-// import React from "react";
-// import RootLayout from "../../../components/Layouts/RootLayout";
+import Card from "@/components/shared/Card";
+import Card2 from "@/components/shared/Card2";
+import React from "react";
+import RootLayout from "../../../components/Layouts/RootLayout";
 
-// const Motherboard2 = ({ motherboards }) => {
-//   return (
-//     <div>
-//       <h1 className="text-3xl p-4">MotherBoard Product</h1>
-//       <div className=" p-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-//         {motherboards?.data.map((service) => (
-//           <Card2 key={service.id} service={service}></Card2>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
+const Motherboard2 = ({ motherboards }) => {
+  return (
+    <div>
+      <h1 className="text-3xl p-4">MotherBoard Product</h1>
+      <div className=" p-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {motherboards?.data.map((service) => (
+          <Card2 key={service.id} service={service}></Card2>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-// export const getServerSideProps = async () => {
-//   // const res = await fetch("http://localhost:3000/api/news"); // internal API connected with mongoDB
-//   const res = await fetch("http://localhost:5000/motherboards"); // --> json server
-//   const data = await res.json();
-//   // console.log(data);
-//   return {
-//     props: {
-//       motherboards: data,
-//       // allNews: data.data, // when using internal API connected with mongoDB
-//     },
-//     // revalidate: 10,
-//   };
-// };
+export const getServerSideProps = async () => {
+  // const res = await fetch("http://localhost:3000/api/news"); // internal API connected with mongoDB
+  const res = await fetch("http://localhost:5000/motherboards"); // --> json server
+  const data = await res.json();
+  // console.log(data);
+  return {
+    props: {
+      motherboards: data,
+      // allNews: data.data, // when using internal API connected with mongoDB
+    },
+    // revalidate: 10,
+  };
+};
 
-// Motherboard2.getLayout = function getLayout(page) {
-//   return <RootLayout> {page} </RootLayout>;
-// };
-// export default Motherboard2;
+Motherboard2.getLayout = function getLayout(page) {
+  return <RootLayout> {page} </RootLayout>;
+};
+export default Motherboard2;
